68bc87844f97b3012e2ad9a1932c704991f3e892
Ciel <imwithye@gmail.com>
2014-06-13 13:32:21 +0800

    import bucket topk impl

---
+++ src/lib/bucket_topk/bucket_topk.h
@@ -0,0 +1,21 @@
+#ifndef BUCKET_TOPK_H_
+#define BUCKET_TOPK_H_
+
+#define THREADS_PER_BLOCK 256
+#define rpc(X) raw_pointer_cast((X).data())
+
+#define BUCKET_TOPK_GAP 1
+#define BUCKET_TOPK_EQUAL(X, Y) ((X-Y)<BUCKET_TOPK_GAP && (X-Y)>0-BUCKET_TOPK_GAP)
+
+typedef struct Bucket {
+	int a_index;
+	int b_index;
+} Bucket;
+
+#include "update_min_max.inc"
+#include "count_sum.inc"
+#include "assign_bucket.inc"
+#include "result_thread_partition.inc"
+#include "save_result.inc"
+#include "bucket_topk.inc"
+#endif
