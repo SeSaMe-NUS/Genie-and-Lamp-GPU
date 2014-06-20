9fa0dc232af7a0d4a318856bc6d20d5a96a1f71c
Ciel <imwithye@gmail.com>
2014-06-20 11:50:42 +0800

    update bucket_topk lib

--- src/Genie/lib/bucket_topk/bucket_topk.h
+++ src/Genie/lib/bucket_topk/bucket_topk.h
@@ -2,6 +2,7 @@
 #define BUCKET_TOPK_H_
 
 #define THREADS_PER_BLOCK 256
+#define BUCKET_TOPK_MAX_K 1000
 #define rpc(X) raw_pointer_cast((X).data())
 
 #define BUCKET_TOPK_GAP 1
@@ -12,6 +13,7 @@ typedef struct Bucket {
 	int b_index;
 } Bucket;
 
+#include "utils.inc"
 #include "update_min_max.inc"
 #include "count_sum.inc"
 #include "assign_bucket.inc"
